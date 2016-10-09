89f8434ea5b163c220250f806067a1a74efae199
Yuhui Li <a@yuhuili.com>
2016-10-09 12:52:50 -0400

    Removed comment, since no additional setup after loading the view needs to be completed at this time

--- cp-tippy/SettingsViewController.swift
+++ cp-tippy/SettingsViewController.swift
@@ -15,8 +15,6 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {

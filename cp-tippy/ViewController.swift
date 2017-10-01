03b94fe33a7c4f61ac42284ea99453d933fc9ffb
Yuhui Li <a@yuhuili.com>
2017-10-01 12:10:49 -0700

    Remove unused code

--- cp-tippy/ViewController.swift
+++ cp-tippy/ViewController.swift
@@ -34,11 +34,6 @@ class ViewController: UIViewController {
         }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func onTap(_ sender: AnyObject) {
         view.endEditing(true)
     }
